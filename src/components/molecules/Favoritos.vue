5597359ff984da1247db0ae95cd5994adc7d6983
John Cotamo <johncotamo@hotmail.com>
2025-03-29 20:51:06 -0500

    se crea pantalla recetas

--- src/components/molecules/Favoritos.vue
+++ src/components/molecules/Favoritos.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="w-full flex justify-between items-center mr-0 ml-0 mt-0 mb-0 pt-4 pr-40 pb-2 pl-12">
       <div>
-        <span class="text-2xl font-normal text-blue-800 border-b-4 border-blue-800 p-2 ">
+        <span class="text-2xl font-normal text-blue-800 border-b-4 border-blue-800 p-2">
           Favoritos
         </span>
         <span class="text-2xl font-normal text-gray-600"> Populares </span>
@@ -10,9 +10,9 @@
     </div>
     <hr class="border-t-4 border-gray-200 mx-12" />
 
-    <div class="flex flex-col items-center  gap-4 p-5 m-0 ">
-      <p class="text-blue-800 font-semibold p-6">¡Sin Favoritos!</p>
-      <ButtomNew nameButton="Añadir" />
+    <div class="flex flex-col items-center justify-center min-h-[600px] p-5 w-full">
+      <p class="text-blue-800 font-semibold mb-6">¡Sin Favoritos!</p>
+      <ButtomNew nameButton="Añadir" class="mx-auto" />
     </div>
   </section>
 </template>

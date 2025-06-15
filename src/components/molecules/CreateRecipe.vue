ce888d393dbaeb356acb54beb11dbce7aa4fb5d7
john cotamo <johncotamo@hotmail.com>
2025-06-15 11:26:23 -0500

    se crea formulario para añadir recetas

--- src/components/molecules/CreateRecipe.vue
+++ src/components/molecules/CreateRecipe.vue
@@ -1,6 +1,27 @@
+
 <template>
+  <div>
+    <h1>Esta Pagina esta en desarrollo</h1>
 
     <div>
-        <h1>Esta Pagina esta en desarrollo</h1>
+      <form action="">
+        <div>
+          <img src="" alt="" />
+          <button>Añadir Imagen</button>
+        </div>
+        <div class="flex flex-col gap-y-2">
+          <label for="Nombre Receta">Name Receta</label>
+          <input type="text" placeholder="Nombre Receta" />
+          <label for="informacion">Ingrese datos de la receta</label>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+       <ButtonNew nameButton="Añadir"/>
+        </div>
+      </form>
     </div>
+  </div>
 </template>
+<script>
+import ButtonNew from '../atoms/ButtomNew.vue';
+
+
+</script>

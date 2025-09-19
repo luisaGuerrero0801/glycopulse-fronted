e9b8b31a56043122e7d4822307aa503348803bfe
John Cotamo <johncotamo@hotmail.com>
2025-09-19 15:07:05 -0500

    se modifica error en doanntes

---
+++ src/layout/LayoutContainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full max-w-screen-2xl p-6 mx-auto overflow-x-hidden">
+    <slot />
+  </div>
+</template>
--- src/views/DonantesView.vue
+++ src/views/DonantesView.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import VistaInicial from '@/components/molecules/NavbarApp.vue'
 import DonantesRh from '@/components/molecules/donors/Donantes.vue'
-import HeaderApp from '@/components/molecules/HeaderApp.vue';
+import HeaderApp from '@/components/molecules/HeaderApp.vue'
+import LayoutContainer from '../layout/LayoutContainer.vue'
 </script>
 
 <template>
   <div class="w-full flex">
+    <!-- Sidebar -->
     <VistaInicial />
+
+   
     <div class="w-full flex-col">
       <HeaderApp pagename="Donantes" />
-      <DonantesRh />
+      <LayoutContainer>
+        <DonantesRh />
+      </LayoutContainer>
     </div>
   </div>
 </template>
